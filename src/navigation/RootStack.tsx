cd1e6a6fdefb9cbe2c6347f30e784325624a43fc
luck_body <707798472@qq.com>
2025-08-12 00:21:45 +0800

    tabbar中增加lottie动画

--- src/navigation/RootStack.tsx
+++ src/navigation/RootStack.tsx
@@ -4,10 +4,10 @@ import {
 } from '@react-navigation/bottom-tabs';
 import React from 'react';
 
-import CustomTabBar from './CustomTabBar';
 import ProfileScreen from '@/screen/profile/ProfileScreen';
 import DetailScreen from '@/screen/home/DetailScreen';
 import HomeStack from './HomeStack';
+import LottieView from 'lottie-react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,19 +15,43 @@ const RootTabNavigator: React.FC = () => {
   return (
     <Tab.Navigator
       initialRouteName="HomeTab"
-      screenOptions={{headerShown: false}}
-      tabBar={({state, descriptors, navigation}: BottomTabBarProps) => (
-        <CustomTabBar
-          state={state}
-          descriptors={descriptors}
-          navigation={navigation}
-        />
-      )}>
-      <Tab.Screen name="HomeTab" component={HomeStack} />
+      screenOptions={{headerShown: false}}>
+      <Tab.Screen
+        name="HomeTab"
+        component={HomeStack}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <LottieView
+              source={
+                focused
+                  ? require('../asset/lottie/lottie-home.json')
+                  : require('../asset/lottie/lottie-home.json')
+              }
+              autoPlay
+              loop={true}
+              style={{width: 40, height: 40}}
+            />
+          ),
+        }}
+      />
       <Tab.Screen
         name="TwoTab"
-        options={{headerShown: true}}
         component={ProfileScreen}
+        options={{
+          headerShown: true,
+          tabBarIcon: ({focused}) => (
+            <LottieView
+              source={
+                focused
+                  ? require('../asset/lottie/lottie-profile.json')
+                  : require('../asset/lottie/lottie-profile.json')
+              }
+              autoPlay
+              loop={true}
+              style={{width: 40, height: 40}}
+            />
+          ),
+        }}
       />
       <Tab.Screen
         name="ThisRouteDoesNotShowBecauseOnlyFourAreInTabs"
